ca08c95c67dbc58d66bfe0f79de763afbb46b0f3
Etienne Dumai <etienne.dumai@gmail.com>
2024-09-13 16:55:03 +0200

    exo 2 commencer mais le mail ne s'envoie pas encore (mauvaise methode) algo surement bon

--- TP2/TP2.php
+++ TP2/TP2.php
@@ -12,28 +12,18 @@
 </html>
     <?php
         Q1();
+       
     ?>
 </body>
 </html>
 <?php
     function Q1(){
-        if(isset($_POST['submit'])){
-            $nom = $_POST['nom'];
-            $age = $_POST['age'];
-            $mail = $_POST['mail'];
-            $don = $_POST['don'];
-            echo "Nom : $nom | Âge : $age | Email : $mail | Don : $don € ";
-            $monFic = fopen("resultats.txt","a+");
-            fwrite($monFic, "Nom : $nom | Âge : $age | Email : $mail | Don : $don € \n");
-            fclose($monFic);
-
-            
-        }
+        
         
             echo '<br><form id="formulaire" method="post">
 
         <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" value = required><br><br>
+        <input type="text" id="nom" name="nom"  required><br><br>
 
         <label for="age">Âge :</label>
         <input type="number" id="age" name="age" min="18"  required><br><br>
@@ -45,7 +35,32 @@
         <input type="number" id="don" name="don" step="0.01" min="1"  required><br><br>
 
         <input type="submit" name="submit" value="Faire un don">
-    </form>';
+    </form>
+    <button id="resultatsMail">Resultats</button><br><br>';
+    if(isset($_POST['submit'])){
+        $nom = $_POST['nom'];
+        $age = $_POST['age'];
+        $mail = $_POST['mail'];
+        $don = $_POST['don'];
+        echo "Nom : $nom | Âge : $age | Email : $mail | Don : $don € ";
+        $monFic = fopen("resultats.txt","a+");
+        fwrite($monFic, "Nom : $nom | Âge : $age | Email : $mail | Don : $don € \n");
+        fclose($monFic);
+
         
     }
+        Q2();
+    }
+    function Q2(){
+        if(isset($_POST['resultatsMail'])){
+            while (!feof($monFic)) {
+                $addrMail=0;
+                $message = fgets($monFic);
+                $messageSplit = explode("|", $message);
+                $mail = explode(":", $messageSplit[2]);
+                mail($mail[$addrMail], "Resultats", $message);
+                $addrMail++;
+            }
+        }
+    };
 ?>
