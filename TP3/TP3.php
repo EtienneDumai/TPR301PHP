fb84d15b54017b79580ee100a191dd13d8d58629
Etienne Dumai <etienne.dumai@gmail.com>
2024-09-25 16:11:33 +0200

    Avancée dans la ligne 30 mais marche pas quand même (image recup mais pas placer dans le dossier

--- TP3/TP3.php
+++ TP3/TP3.php
@@ -22,7 +22,35 @@
         for ($i=0; $i < $uploadCount ; $i++) { 
             echo "<input type='file' name='photo$i' id='photo$i'><br>";
         }
+        echo "<input type='submit' value='Envoyer les fichiers' name='submitPhotos'>";
         echo "</form>";
-    }
     
+    }
+    if (isset($_POST['submitPhotos'])) {
+        
+        
+        $uploadDir = 'upload/';
+        //creer le dossier si il n'existe pas
+        if(!is_dir($uploadDir)){
+            mkdir($uploadDir, 0777, true);
+        }
+        var_dump($uploadDir);
+        for ($i=0; $i < $uploadCount; $i++) { 
+            $nomImage = 'file' . $i;
+            if (isset($_FILES[$nomImage])) {
+                $currentFile = $_FILES[$nomImage];
+                $fileName = basename($currentFile['name']);
+                if(move_uploaded_file($currentFile, $uploadDir . $filename)) {
+                    echo "Photo $i : $filename uploadée avec succès.<br>";
+                } 
+                else {
+                    echo "Erreur lors de l'upload de la photo $i.<br>";
+                }
+            }
+            else {
+                echo "Erreur avec la photo $i.<br>";
+            }
+        }
+    }
+
 ?>
